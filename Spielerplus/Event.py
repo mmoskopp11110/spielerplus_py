e542eee9c277246f4dee6e0e01b2d4241136c311
Markus Moskopp <markus.moskopp@web.de>
2019-09-07 18:47:30 +0200

    Add ifttt integration

--- Spielerplus/Event.py
+++ Spielerplus/Event.py
@@ -2,8 +2,11 @@ class SpielerplusEvent:
     def __init__(self, event_id, event_type):
         self.eid = event_id
         self.etype = event_type
+        self.name = ""
         self.date = ""
-        self.time = ""
+        self.start = ""
+        self.end = ""
+        self.isTracked = False
         self.participants = {}
         self.absents = {}
         self.unassigned = {}
--- Spielerplus/Helper.py
+++ Spielerplus/Helper.py
@@ -5,6 +5,8 @@ import re
 from Spielerplus.User import SpielerplusUser
 from Spielerplus.Event import SpielerplusEvent
 import jsonpickle
+import py_linq
+import time
 
 
 class SpielerplusHelper:
@@ -20,6 +22,7 @@ class SpielerplusHelper:
             'participating': 2,
             'unsafe': 3
         }
+        self.ifttt_url = 'https://maker.ifttt.com/trigger/spielerplus/with/key/bpw1Uqr9MJpEDnjhcBvN-e'
 
     def login(self, mail, password):
         if self.isLoggedIn:
@@ -67,20 +70,31 @@ class SpielerplusHelper:
         events = []
         # get general event info
         for event in event_list:
-            event_link_list = event.select('div .event-header-border')
-            for event_link in event_link_list:
-                if 'href' in event_link.attrs:
-                    try:
-                        link = event_link.attrs['href']
-                        event_id = re.search('[0-9]+', link).group(0)
-                        if event_id in self.events:
-                            continue
-                        event_type = re.search('/[a-z]+/', link).group(0).replace('/', '')
-                        new_event = SpielerplusEvent(event_id, event_type)
-                        events.append(new_event)
-                        self.events[event_id] = new_event
-                    except:
-                        pass
+            if 'id' in event.attrs:
+                idstring = event.attrs['id']
+                event_id = re.search('[0-9]+', idstring).group(0)
+                try:
+                    if event_id in self.events:
+                        continue
+                    event_type = re.search('event-[a-z]+', idstring).group(0).replace('event-', '')
+                    new_event = SpielerplusEvent(event_id, event_type)
+
+                    # get event name
+                    new_event.name = event.select('.panel-title')[1].text
+
+                    # times
+                    times = event.select('.event-time-label + .event-time-value')
+                    new_event.start = times[1].text.replace('\n', '').strip()
+                    new_event.end = times[2].text.replace('-:-', '').replace('\n', '').strip()
+                    if new_event.end == '':
+                        new_event.end = '23:00 Uhr'
+
+                    events.append(new_event)
+                    self.events[event_id] = new_event
+                except:
+                    pass
+            else:
+                continue
 
         # get event details
         for event in events:
@@ -90,11 +104,10 @@ class SpielerplusHelper:
             participation_reply = json.loads(participation_req.text)['html'].replace('\n', '')
             participation_modal = BeautifulSoup(participation_reply, 'html.parser')
 
-            # event time and date
+            # event date
             event_header_subline = participation_modal.select('.participation-header .subline')
-            datetimestring = event_header_subline[0].text
-            # todo: parse date and time
-            event.date = datetimestring
+            datetimestring = event_header_subline[0].text.strip()
+            event.date = re.search('^[0-9.]+', datetimestring).group(0)
 
             # not assigned users
             self.get_participation(event, participation_modal, self.list_modes['unassigned'])
@@ -120,6 +133,12 @@ class SpielerplusHelper:
             selector = '#Unsi'
         else:
             return
+
+        # clear participation
+        event.participants = {}
+        event.absents = {}
+        event.unassigned = {}
+        event.unsafe = {}
 
         participation_list = modal_soup.select('{} .participation-list-user'.format(selector))
         for user in participation_list:
@@ -148,6 +167,34 @@ class SpielerplusHelper:
                 pass
 
     def load_events(self, filename):
-        with open(filename, 'r') as f:
-            jsonstring = f.read().replace('\n', '')
-            self.events = jsonpickle.decode(jsonstring)
+        try:
+            with open(filename, 'r') as f:
+                jsonstring = f.read().replace('\n', '')
+                self.events = jsonpickle.decode(jsonstring)
+        except FileNotFoundError:
+            pass
+
+    def track_events(self, name):
+        for event in self.events:
+            ev = self.events[event]
+            if ev.isTracked:
+                continue
+            totrack = {**ev.unassigned, **ev.unsafe}
+            doTrack = py_linq.Enumerable(totrack.values()).where(lambda u: u.name == name).first_or_default()
+            if doTrack:
+                if ev.end == '':
+                    ev.end = '23:00 Uhr'
+                data = {
+                    'value1': format_datetime(ev.date, ev.start),
+                    'value2': format_datetime(ev.date, ev.end),
+                    'value3': ev.name
+                }
+                r = requests.post(self.ifttt_url, data=data)
+                if r.ok:
+                    ev.isTracked = True
+
+
+def format_datetime(date, hour):
+    dtstr = '{} {}'.format(date, hour)
+    t = time.strptime(dtstr, '%d.%m.%y %H:%M Uhr')
+    return time.strftime('%Y-%m-%dT%H:%MTZD', t)
